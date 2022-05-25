5d5f886938292bc827081e3a423f748e8c2153a2
alexg-lviv <alexg.lviv@gmail.com>
2022-05-25 16:47:53 +0300

    add json server

--- twister/src/components/tweets/TweetManager.js
+++ twister/src/components/tweets/TweetManager.js
@@ -1,24 +1,36 @@
-import { useState } from "react";
+import {useEffect, useState} from "react";
 import TweetField from "./TweetField";
 import Tweet from "./Tweet";
 
 const TweetManager = () => {
     const [tweets, setTweets] = useState([]);
 
+    useEffect(() => {
+        fetch('http://localhost:3001/tweets')
+            .then(response => response.json())
+            .then(tweets => setTweets(tweets))
+    }, [])
+
     const onTweetInpSend = (value) => {
-        setTweets((existingTweets) => [...existingTweets, { value, id: new Date().getTime(), likes: 0, dislikes: 0 }])
+        const newTweet = { value, id: new Date().getTime(), likes: 0, dislikes: 0 };
+        setTweets((existingTweets) => [...existingTweets, newTweet]);
+        fetch('http://localhost:3001/tweets', {method: "POST", body: JSON.stringify(newTweet), headers:{'content-type': 'application/json'}});
     }
 
     const onRemove = (id) => {
         setTweets((existingTweets) => existingTweets.filter(tweet => tweet.id !== id))
+        fetch('http://localhost:3001/tweets/' + id, {method: "DELETE"})
     }
 
     const onDislike = (id, dislikes) => {
         setTweets((existingTweets) => existingTweets.map(tweet => tweet.id === id ?({...tweet, dislikes: dislikes + 1}): tweet))
+        fetch('http://localhost:3001/tweets/' + id, {method: "PATCH", body: JSON.stringify({'dislikes': dislikes+1}), headers:{'content-type': 'application/json'}});
+
     }
 
     const onLike = (id, likes) => {
         setTweets((existingTweets) => existingTweets.map(tweet => tweet.id === id ?({...tweet, likes: likes + 1}): tweet))
+        fetch('http://localhost:3001/tweets/' + id, {method: "PATCH", body: JSON.stringify({'likes': likes+1}), headers:{'content-type': 'application/json'}});
     }
 
     return <div>
