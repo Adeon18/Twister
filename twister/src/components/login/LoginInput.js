1712f7c3b102f5411ee417b0a8dbd90f625ca25a
Adeon18 <ostaptrush18@gmail.com>
2022-06-03 23:12:53 +0300

    formatted all the files

--- twister/src/components/login/LoginInput.js
+++ twister/src/components/login/LoginInput.js
@@ -1,21 +1,21 @@
-import {useState} from "react"
-import {Link} from "react-router-dom";
-import {useNavigate} from "react-router";
+import { useState } from "react"
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router";
 
-const LoginInputForm = ({Login, Register, error}) => {
+const LoginInputForm = ({ Login, Register, error }) => {
 
     const [login, setLogin] = useState("");
     const [password, setPassword] = useState("");
 
     const submitLoginHandler = ev => {
         ev.preventDefault();
-        Login({login: login, password: password});
+        Login({ login: login, password: password });
     }
 
 
     const submitRegisterHandler = ev => {
         ev.preventDefault();
-        Register({login: login, password: password, id: Math.round(new Date().getTime() / 1000)});
+        Register({ login: login, password: password, id: Math.round(new Date().getTime() / 1000) });
     }
 
     return (
@@ -28,13 +28,13 @@ const LoginInputForm = ({Login, Register, error}) => {
                 <div className="form-input">
                     <label htmlFor="login">Login:</label>
                     <input type="text" name="login" id="login" onChange={ev => setLogin(ev.target.value)} value={login}
-                           maxLength="30"/>
+                        maxLength="30" />
                 </div>
 
                 <div className="form-input">
                     <label htmlFor="password">Password:</label>
                     <input type="password" name="password" id="password" onChange={ev => setPassword(ev.target.value)}
-                           value={password} maxLength="30"/>
+                        value={password} maxLength="30" />
                 </div>
                 <input type="submit" name="login" value="Log In"></input>
                 <button onClick={submitRegisterHandler} name="register">Register</button>
